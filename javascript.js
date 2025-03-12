e609dc31585c029cfb3d7fb8929ebbfaf368f367
cwilliams0926 <coopwilliams03@hotmail.com>
2025-03-12 10:21:36 -0400

    Add logic for playRound()

--- javascript.js
+++ javascript.js
@@ -23,15 +23,25 @@ computerScore = 0;
 
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
+console.log(humanSelection);
+console.log(computerSelection);
 
 function playRound(humanChoice, computerChoice) {
-    // Make humanChoice lowercase
-    // Make computerChoice lowercase
-    // if they are the same string, tie
-    // if humanChoice is rock and computerChoice is scissors, win
-    // if humanChoice is paper and computerChoice is rock, win
-    // if humanChoice is scissors and computerChoice is paper, win
-    // else, lose
+    if(humanChoice.toLowerCase() == computerChoice.toLowerCase()) {
+        console.log("Draw! You chose the same choice as the computer");
+    }
+    else if(humanChoice.toLowerCase() == "rock" && computerChoice.toLowerCase() == "scissors") {
+        console.log("You win! Rock beats scissors");
+    }
+    else if(humanChoice.toLowerCase() == "paper" && computerChoice.toLowerCase() == "rock") {
+        console.log("You win! Paper beats rock!");
+    }
+    else if(humanChoice.toLowerCase() == "scissors" && computerChoice.toLowerCase() == "paper") {
+        console.log("You win! Scissors beats paper");
+    }
+    else {
+        console.log("You lose! " + computerChoice + " beats " + humanChoice.toLowerCase());
+    }
 }
 
 playRound(humanSelection, computerSelection);
