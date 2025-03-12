419a51ec837245f75a7144d7d33d3249c93c7a41
cwilliams0926 <coopwilliams03@hotmail.com>
2025-03-12 11:05:31 -0400

    Move score vars and playRound into playGame()

--- javascript.js
+++ javascript.js
@@ -18,30 +18,31 @@ function getHumanChoice() {
     return choice;
 }
 
-humanScore = 0;
-computerScore = 0;
-
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 console.log(humanSelection);
 console.log(computerSelection);
 
-function playRound(humanChoice, computerChoice) {
-    if(humanChoice.toLowerCase() == computerChoice.toLowerCase()) {
-        console.log("Draw! You chose the same choice as the computer");
-    }
-    else if(humanChoice.toLowerCase() == "rock" && computerChoice.toLowerCase() == "scissors") {
-        console.log("You win! Rock beats scissors");
-    }
-    else if(humanChoice.toLowerCase() == "paper" && computerChoice.toLowerCase() == "rock") {
-        console.log("You win! Paper beats rock!");
-    }
-    else if(humanChoice.toLowerCase() == "scissors" && computerChoice.toLowerCase() == "paper") {
-        console.log("You win! Scissors beats paper");
-    }
-    else {
-        console.log("You lose! " + computerChoice + " beats " + humanChoice.toLowerCase());
+playRound(humanSelection, computerSelection);
+
+function playGame() {
+    humanScore = 0;
+    computerScore = 0;
+    function playRound(humanChoice, computerChoice) {
+        if(humanChoice.toLowerCase() == computerChoice.toLowerCase()) {
+            console.log("Draw! You chose the same choice as the computer");
+        }
+        else if(humanChoice.toLowerCase() == "rock" && computerChoice.toLowerCase() == "scissors") {
+            console.log("You win! Rock beats scissors");
+        }
+        else if(humanChoice.toLowerCase() == "paper" && computerChoice.toLowerCase() == "rock") {
+            console.log("You win! Paper beats rock!");
+        }
+        else if(humanChoice.toLowerCase() == "scissors" && computerChoice.toLowerCase() == "paper") {
+            console.log("You win! Scissors beats paper");
+        }
+        else {
+            console.log("You lose! " + computerChoice + " beats " + humanChoice.toLowerCase());
+        }
     }
 }
-
-playRound(humanSelection, computerSelection);
